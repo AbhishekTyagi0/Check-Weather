0ca556c4e7289de0264fe65e1c46e99ad5211af9
AbhishekTyagi0 <abhishektyagigi4310@gmail.com>
2023-11-05 21:06:16 +0530

    Created a simple weather app using api and fetching data from it

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function mainfunction() {
   const locationName = document.querySelector(".location-name");
   const temperature = document.querySelector(".temp>h3");
+  const time = document.querySelector(".time");
   const input = document.querySelector(".search-box>input");
   const button = document.querySelector(".search-box>button");
+  const condition = document.querySelector('.PHWind>p:first-child');
+  const humidity = document.querySelector('.PHWind>p:nth-child(2)');
+  const wind = document.querySelector('.PHWind>p:last-child');
 
   fetch(
     `http://api.weatherapi.com/v1/forecast.json?key=329d61e18fda465e918165250230211&q=07112&days=7`,
@@ -17,8 +21,6 @@ function mainfunction() {
       let region = response.location.name;
       locationName.textContent = region;
 
-      let temp = response.current.temp_c;
-      temperature.textContent = temp;
     });
 
   function inputFunction() {
@@ -34,9 +36,31 @@ function mainfunction() {
       .then(function (response) {
         console.log(response);
 
-        let searchLocation = response.location.name;
+        let searchLocation = response.location.name +", "+ response.location.country.slice(0,2);
         locationName.textContent = searchLocation;
-      });
+
+        let temp = response.current.temp_c + " \u00B0C";
+        temperature.textContent = temp;
+
+        let cond = response.current.condition.text;
+        condition.textContent = `Condition: ${cond}`;
+        
+        let humidityRange = response.current.humidity;
+        humidity.textContent = `Humidity: ${humidityRange}%`;
+
+        let windSpeed = response.current.wind_kph;
+        wind.textContent = `Wind Speed: ${windSpeed} Km/H`;
+
+        let timeZone = response.location.localtime;
+        time.textContent = `Time: ${timeZone}`;
+
+
+      })
+
+      .catch(function(err){
+
+        console.log(`Error: ${err}`);
+      })
   }
 
   button.addEventListener("click", inputFunction);
